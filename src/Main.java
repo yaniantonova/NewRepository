a82a76a0efbf0d2f92b30a798b53668d8d213bfa
Yana_Antonova <yanagromchenko@gmail.com>
2022-11-06 13:36:27 +0200

    add 2 new objects Lecture in main

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
 package com.school;
 
 import com.school.models.Lecture;
+
 public class Main {
     public static void main(String[] args) {
         Lecture Lecture = new Lecture();
         Lecture secondLecture = new Lecture();
         Lecture thirdLecture = new Lecture();
+        Lecture fourthLecture = new Lecture()
+        Lecture fifthLecture = new Lecture();
 
         System.out.println(Lecture.counterLecture);
     }
